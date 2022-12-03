c8c1dacb17e3846d1e444117e182d154cd9821c6
omarali1997 <omar.aziz.9718@gmail.com>
2022-12-03 19:31:48 +0000

    lab20

--- snacks/urls.py
+++ snacks/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
-from .views import SnackListView
+from .views import SnackListView,SnackDetail
 urlpatterns = [
-    path('',SnackListView.as_view(),name='snacks')
+    path('',SnackListView.as_view(),name='snacks'),
+    path('<int:pk>',SnackDetail.as_view(),name='details')
+
 ]
--- snacks/views.py
+++ snacks/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView,DetailView
 from .models import Snack
 
 # Create your views here.
@@ -7,3 +7,7 @@ from .models import Snack
 class SnackListView(ListView):
     template_name='snack_list.html'
     model=Snack
+
+class SnackDetail(DetailView):
+    model=Snack
+    template_name='snack_detail.html'
